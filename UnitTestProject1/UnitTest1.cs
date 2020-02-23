b2637cb289129e4330e318b39e9b36e22f1048f9
Rashmidhakne <rash91d@gmail.com>
2020-02-23 20:28:07 +0530

    added files

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using simpleregistration;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            tutorial tp = new tutorial();
+            Assert.AreEqual(tp.name, "hello dude");
+        }
+    }
+}
